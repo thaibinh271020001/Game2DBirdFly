885744ea8e1d53634269b302325b7be5c761538c
thaibinh271020001 <dangthaibinh10a4@gmail.com>
2023-04-11 22:09:19 +0700

    comit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float Jumpspeed = 8f;
+    public float direction = 0f;
+    public Rigidbody2D player;
+
+    /*public Transform groundCheck;
+    public float groundCheckRadius = 0f;
+    public LayerMask groundLayer;
+    public bool isToochingGround;*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+        /*isToochingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);*/
+
+
+        player.velocity = new Vector2(direction * speed, player.velocity.y);
+
+        if (Input.GetButtonDown("Jump") /*&& isToochingGround*/)
+        {
+            player.velocity = new Vector2(player.velocity.x, Jumpspeed);
+        }
+
+        
+    }
+}
